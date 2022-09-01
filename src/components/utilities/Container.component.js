18d15fc13f3315dca46d53a83ced0171f2b3429e
Swabhimaan Dash <dashswabhimaan@gmail.com>
2022-09-01 22:47:16 +0530

    [feat] : Responsive container component

--- src/components/utilities/Container.component.js
+++ src/components/utilities/Container.component.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Container = ({ children }) => {
-  return <div className="max-w-7xl mx-auto px-10">{children}</div>;
+  const desktop = "max-w-7xl px-10";
+  const laptop = "xl:max-w-5xl xl:px-8";
+  const tabletLandscape = "lg:max-w-3xl lg:px-6";
+  const tablet = "md:max-w-full md:px-4";
+  const mobile = "sm:max-w-full sm:px-2";
+  
+  return (
+    <div
+      className={`mx-auto ${desktop} ${laptop} ${tabletLandscape} ${tablet} ${mobile}}`}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Container;
