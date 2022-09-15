581f077a779e57a1adf1deaa738db4fc75a4d72f
Swabhimaan Dash <dashswabhimaan@gmail.com>
2022-09-15 22:57:01 +0530

    [fix] : [issue-3] : Fixed the Unnecessary blank space issue

--- src/components/features/Recipes/Recipes.component.js
+++ src/components/features/Recipes/Recipes.component.js
@@ -18,7 +18,7 @@ const Recipes = () => {
 
   return (
     <Container>
-      <div className="grid grid-cols-3 lg:grid-cols-2 sm:grid-cols-1 gap-8 mt-10">
+      <div className="grid grid-cols-3 lg:grid-cols-2 sm:grid-cols-1 gap-8 mt-10 items-start">
         {_.map(
           recipes,
           ({
