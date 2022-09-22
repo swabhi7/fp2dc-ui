6e92776ba707d8803b705ed995d199b18a8b6944
Swabhimaan Dash <dashswabhimaan@gmail.com>
2022-09-22 22:39:29 +0530

    [chore] : Commented some concerns to be checked in future

--- src/components/features/Recipes/Rating.component.js
+++ src/components/features/Recipes/Rating.component.js
@@ -2,12 +2,14 @@ import React from "react";
 import _ from "lodash";
 import { FaRegStar, FaStar, FaStarHalfAlt } from "react-icons/fa";
 
+// NOTE - What exactly should be input here to make this component more reusable?
 const Rating = ({ reviews }) => {
   if (_.isEmpty(reviews)) return;
 
   const rating = _.meanBy(reviews, "rating");
   // const numberOfReviews = _.size(reviews);
 
+  // NOTE - Should below calculation logic be moved to another component or even API side?
   const fullStars = _.floor(rating);
   const halfStars = rating - _.floor(rating) === 0 ? 0 : 1;
   const emptyStars = 5 - fullStars - halfStars;
@@ -15,6 +17,7 @@ const Rating = ({ reviews }) => {
   return (
     <div>
       {_.map(Array(fullStars), (value, index) => (
+        // NOTE - Is there any issue with using key as index? Check.
         <FaStar key={index} className="inline-block" />
       ))}
       {_.map(Array(halfStars), (value, index) => (
