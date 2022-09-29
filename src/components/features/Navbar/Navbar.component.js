222acf55cbe719285fb44724da55c9a46a86117d
Swabhimaan Dash <dashswabhimaan@gmail.com>
2022-09-29 23:34:49 +0530

    [style/feat] : Initial CSS

--- src/components/features/Navbar/Navbar.component.js
+++ src/components/features/Navbar/Navbar.component.js
@@ -1,16 +1,22 @@
 import React from "react";
-import { FaSearch } from "react-icons/fa";
+import { FaBars, FaSearch } from "react-icons/fa";
+import classes from "./Navbar.module.css";
 
 const Navbar = () => {
   return (
-    <div>
-      <div>&lt;FP2DC /&gt;</div>
-      <div>
-        <input type="text" placeholder="Search" />
-        <FaSearch />
+    <div className={classes["navbar"]}>
+      <div className={classes["logo"]}>&lt; FP2DC / &gt;</div>
+      <div className={classes["search"]}>
+        <input
+          type="text"
+          placeholder="Search"
+          className={classes["search-box"]}
+        />
+        <FaSearch className={classes["search-icon"]} />
       </div>
-      <div>
-        <ul>
+      <div className={classes["menu"]}>
+        <FaBars className={classes["menu-icon"]} />
+        <ul className={classes["menu-list"]}>
           <li>Recipes</li>
           <li>Chef Says</li>
           <li>About</li>
