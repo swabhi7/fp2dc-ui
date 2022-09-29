2b640eaf16accbeec600da9eb682f136a7883d48
Swabhimaan Dash <dashswabhimaan@gmail.com>
2022-09-29 23:03:13 +0530

    [feat] : Navbar markup

--- src/components/features/Navbar/Navbar.component.js
+++ src/components/features/Navbar/Navbar.component.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { FaSearch } from "react-icons/fa";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <div>&lt;FP2DC /&gt;</div>
+      <div>
+        <input type="text" placeholder="Search" />
+        <FaSearch />
+      </div>
+      <div>
+        <ul>
+          <li>Recipes</li>
+          <li>Chef Says</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Sign In</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
