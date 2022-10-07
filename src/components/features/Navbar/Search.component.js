7a0ce40a0c9fb3719a7796f2f54f71a8631e89cc
Swabhimaan Dash <dashswabhimaan@gmail.com>
2022-10-07 19:24:31 +0530

    [feat/fix] : Do NOT show modal when searchbox is already visible (On navbar)

--- src/components/features/Navbar/Search.component.js
+++ src/components/features/Navbar/Search.component.js
@@ -1,7 +1,20 @@
+import { useEffect, useRef, useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import classes from "./Search.module.css";
+import _ from "lodash";
 
 const Search = ({ forseExpand = false, searchExpandHandler }) => {
+  const searchBoxRef = useRef();
+  const [isSearchBoxExpanded, setIsSearchBoxExpanded] = useState(
+    _.get(searchBoxRef, ["current", "offsetWidth"], 0) !== 0
+  );
+
+  useEffect(() => {
+    setIsSearchBoxExpanded(
+      _.get(searchBoxRef, ["current", "offsetWidth"], 0) !== 0
+    );
+  }, [searchBoxRef]);
+
   return (
     <div
       // If forseExpand is true then it means this will be inside modal, so change the styling accordingly
@@ -12,6 +25,7 @@ const Search = ({ forseExpand = false, searchExpandHandler }) => {
       }`}
     >
       <input
+        ref={searchBoxRef}
         type="text"
         placeholder="Search"
         className={`${
@@ -21,7 +35,7 @@ const Search = ({ forseExpand = false, searchExpandHandler }) => {
         }`}
       />
       <FaSearch
-        onClick={searchExpandHandler}
+        onClick={isSearchBoxExpanded ? null : searchExpandHandler}
         className={`${
           forseExpand === false
             ? classes["search-icon"]
