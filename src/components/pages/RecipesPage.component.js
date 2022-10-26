d5de34784d70099b8bcaf7014af54b7290dd1de1
Swabhimaan Dash <dashswabhimaan@gmail.com>
2022-10-26 20:51:23 +0530

    [feat] : Logic moved to parent page component

--- src/components/pages/RecipesPage.component.js
+++ src/components/pages/RecipesPage.component.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchRecipes } from "../../store/actionDispatchers/recipes.actionDispatcher";
 import Recipes from "../features/Recipes/Recipes.component";
 
 const RecipesPage = () => {
-  return <Recipes />;
+  const { recipes } = useSelector((store) => store.recipes);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    (async () => {
+      dispatch(fetchRecipes());
+    })();
+  }, [dispatch]);
+  
+  return <Recipes recipes={recipes} />;
 };
 
 export default RecipesPage;
