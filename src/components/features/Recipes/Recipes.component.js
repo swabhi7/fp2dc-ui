9af86d28189c8bd9e4e4374eb4b318a1cde4bbf7
Swabhimaan Dash <dashswabhimaan@gmail.com>
2022-10-27 19:34:16 +0530

    [refactor] : Moved Container to page

--- src/components/features/Recipes/Recipes.component.js
+++ src/components/features/Recipes/Recipes.component.js
@@ -1,41 +1,38 @@
 import _ from "lodash";
 import React from "react";
-import Container from "../../utilities/Container.component";
 import Recipe from "./Recipe.component";
 import classes from "./Recipes.module.css";
 
 const Recipes = ({ recipes = [] }) => {
   return (
-    <Container>
-      <div className={classes["recipes"]}>
-        {_.map(
-          recipes,
-          ({
-            _id,
-            imageUrl,
-            title,
-            description,
-            cookingTime,
-            difficulty,
-            serves,
-            tags,
-            reviews,
-          }) => (
-            <Recipe
-              key={_id}
-              imageUrl={imageUrl}
-              title={title}
-              description={description}
-              cookingTime={cookingTime}
-              difficulty={difficulty}
-              serves={serves}
-              tags={tags}
-              reviews={reviews}
-            />
-          )
-        )}
-      </div>
-    </Container>
+    <div className={classes["recipes"]}>
+      {_.map(
+        recipes,
+        ({
+          _id,
+          imageUrl,
+          title,
+          description,
+          cookingTime,
+          difficulty,
+          serves,
+          tags,
+          reviews,
+        }) => (
+          <Recipe
+            key={_id}
+            imageUrl={imageUrl}
+            title={title}
+            description={description}
+            cookingTime={cookingTime}
+            difficulty={difficulty}
+            serves={serves}
+            tags={tags}
+            reviews={reviews}
+          />
+        )
+      )}
+    </div>
   );
 };
 
--- src/components/pages/RecipesPage.component.js
+++ src/components/pages/RecipesPage.component.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchRecipes } from "../../store/actionDispatchers/recipes.actionDispatcher";
 import Recipes from "../features/Recipes/Recipes.component";
+import Container from "../utilities/Container.component";
 
 const RecipesPage = () => {
   const { recipes } = useSelector((store) => store.recipes);
@@ -13,8 +14,12 @@ const RecipesPage = () => {
       dispatch(fetchRecipes());
     })();
   }, [dispatch]);
-  
-  return <Recipes recipes={recipes} />;
+
+  return (
+    <Container>
+      <Recipes recipes={recipes} />
+    </Container>
+  );
 };
 
 export default RecipesPage;
