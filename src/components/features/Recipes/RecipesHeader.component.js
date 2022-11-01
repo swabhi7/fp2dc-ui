2cd8bf78f1b97d350e6dfc1d0ed7a1d875ce6aed
Swabhimaan Dash <dashswabhimaan@gmail.com>
2022-11-01 20:27:53 +0530

    [feat] : Removed Sorting and ItemsPerPage for now - As per client request - removed itemsperpage - Sorting will be done in some other hash

--- src/components/features/Recipes/RecipesHeader.component.js
+++ src/components/features/Recipes/RecipesHeader.component.js
@@ -1,15 +1,9 @@
 import React from "react";
-import ItemsPerPage from "./ItemsPerPage.component";
 import Pagination from "./Pagination.component";
-import Sorting from "./Sorting.component";
 
 const RecipesHeader = () => {
   return (
     <div className="mt-10">
-      <div className="flex justify-center gap-4 mb-4">
-        <Sorting />
-        <ItemsPerPage />
-      </div>
       <Pagination currentPage={7} totalPages={18} maxNumberOfPageButtons={5} />
     </div>
   );
